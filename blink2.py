454f41932cf988149c9c45dcd508145888f73474
jlophor1903 <jlophor1903@g.educaand.es>
2025-01-16 12:48:19 +0100

    trabajo del jueves 16

--- blink2.py
+++ blink2.py
@@ -3,27 +3,34 @@ import time
 
 funcionando = True      #esta variable la vamos a usar para parar pygame cuando terminemos
 estado = False        #esta variable es para ir cambiando el color del circulo.
-
+color = [(220,220,0), (50,50,50)]
 pg.init()
 screen = pg.display.set_mode((500,500),0 ,32 )
-
+tiempoInicial = time.process_time()*1000
 
 while funcionando:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             funcionando = False
             break
-
-    if estado:
+    tiempoActual = time.process_time()*1000
+    if (tiempoActual - tiempoInicial) > 500:
+        """
+        if estado:
             color = [200,200,0]
             estado = False
-    else: 
+        else: 
             color =  [50,50,50]
             estado = True
-
+            """
+        tiempoInicial = time.process_time()*1000
+        estado = not(estado)
+    
     screen.fill([100, 100, 100])  
-    pg.draw.circle(screen, color ,(200,200), 70) 
+    pg.draw.circle(screen, color[estado] ,(200,200), 70) 
     pg.display.flip()
-    time.sleep(1)
+
+for i in range(10000000):
+     continue 
 
 pg.quit()   
--- blink3.py
+++ blink3.py
@@ -1,29 +1,36 @@
 import pygame as pg 
-import time
+from threading import Timer
 
 funcionando = True      #esta variable la vamos a usar para parar pygame cuando terminemos
 estado = False        #esta variable es para ir cambiando el color del circulo.
-
+color = [(220,220,0), (50,50,50)]
 pg.init()
 screen = pg.display.set_mode((500,500),0 ,32 )
 
+def blink():
+    global estado
+    global t
+    estado = not(estado)
+    t = Timer(1, blink)
+    t.start()
+
+t = Timer(1, blink) 
+t.start()   
 
 while funcionando:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             funcionando = False
             break
-
-    if estado:
-            color = [200,200,0]
-            estado = False
-    else: 
-            color =  [50,50,50]
-            estado = True
-
+  
+        estado = not(estado)
+    
     screen.fill([100, 100, 100])  
-    pg.draw.circle(screen, color ,(200,200), 70) 
+    
+    pg.draw.circle(screen, color[estado] ,(200,200), 70) 
     pg.display.flip()
-    time.sleep(1)
+
+
+   
 
 pg.quit()   
